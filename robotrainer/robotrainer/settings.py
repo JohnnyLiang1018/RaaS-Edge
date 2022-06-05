35b42f064a47cf7acc3374ac4f62171a4b677895
Inderjit Bassi <inderjit.bassi@sjsu.edu>
2022-06-05 15:20:31 -0700

    Add connection to RDS DB

--- robotrainer/robotrainer/settings.py
+++ robotrainer/robotrainer/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(find_dotenv())
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'robotrainer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_IDENTIFIER'),
+        'USER': os.environ.get('MYSQL_USERNAME'),
+        'PASSWORD': os.environ.get('MYSQL_PWD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': 3306
     }
 }
 
